8c864fea7f13bcde7939022e5c08ea076c4efe37
rogeliogarciardz <rogeliogarciardz@gmail.com>
2023-03-17 11:18:37 -0600

    agregar 'omitnan' en las operaciones

--- analisis_ndvi/m_reglin.m
+++ analisis_ndvi/m_reglin.m
@@ -1,9 +1,9 @@
 function [a0,a1,yajuste] = m_reglin(x,y)
 %fitlm
     if(length(x)==length(y))
-        n =length(x);
+        n = length(x);
 
-        a1 = ( (n * sum(x.*y)) - (sum(x)*sum(y) ))/ ( n * sum(x.^2) - sum(x)^2);
+        a1 = ( (n * sum(x.*y,"omitnan")) - (sum(x,"omitnan")*sum(y,"omitnan") ))/ ( n * sum(x.^2,"omitnan") - sum(x,"omitnan")^2);
         a0 = mean(y,'omitnan') - a1 * mean(x,'omitnan');
         yajuste = a0+a1*x;
 
