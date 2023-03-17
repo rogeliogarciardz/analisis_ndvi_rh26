29905133a490cb58211f1ce6f8a29a1431d2fd04
rogeliogarciardz <rogeliogarciardz@gmail.com>
2023-03-17 11:18:13 -0600

    17 mar 23

--- main.m
+++ main.m
@@ -1,9 +1,15 @@
 %% Código principal
+import matlab.io.hdfeos.*
+clearvars -except area_estudio;
+close all;
 
-disp("Saludos");
+declaraciones
 
-figure;
+% obtener la info del área de estudio
+[lat,lon,ndvi] = m_leer_dir_hdfs(dir_data,coord_1k_v6_inicio,coord_1k_v6_tam,coord_1k_v7_inicio,coord_1k_v7_tam);
+info_hdf = m_infohdfs2table(dir_data);
 
-% dibujar el mapa del promedio de todos los años
-m_dibujar_mapa_ndvi(lon_mapa,lat_mapa,lon,lat,m1,"NDVI promedio del 2000 al 2020",0);
-m_dibujar_otras_areas(dir_data);
+ndvi(ndvi<=0)=nan;
+
+m_dibujar_mapa(lon_mapa,lat_mapa,lon,lat,ndvi,"NDVI",[],ndvi_colormap,"NDVI");
+m_dibujar_kml(dir_data,"RH26",1,'b',"RH26");
