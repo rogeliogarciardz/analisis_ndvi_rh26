95a9b36cc00a2c87e5e0236f7a4d7e09340ba576
rogeliogarciardz <rogeliogarciardz@gmail.com>
2023-09-19 12:35:36 -0500

    19/09/2023

--- analisis_ndvi/m_dibujar_mapa.m
+++ analisis_ndvi/m_dibujar_mapa.m
@@ -1,33 +1,49 @@
-function m_dibujar_mapa(lon_mapa,lat_mapa,lon,lat,datos,~,pclim,ppcolor,plabel)
+function m_dibujar_mapa(lon_mapa,lat_mapa,lon,lat,datos,titulo,pclim,ppcolor,plabel)
     %% dibujar mapa ndvi proyeccion mercator 
-    m_proj('mercator','long',lon_mapa,'lat',lat_mapa);
+    m_proj('mercator','lon',lon_mapa,'lat',lat_mapa);
 
     %% dibujar linea de costa con 'intermediate' resolution 
-    m_gshhs_i('patch',[0.7 0.7 0.7]);
+    %m_gshhs_i('patch',[0.7 0.7 0.7]);
     
     %%dibujar el mapa
-    m_pcolor(lon,lat,datos); %shading interp ;
-    colorbar('eastoutside');
+    m_pcolor(lon,lat,datos); shading interp ;
 
     colormap(ppcolor);
 
+    if plabel ~= ""    
+        colorbar('eastoutside');
+        c = colorbar;
+        c.Label.String = plabel;
+    end
+
     if length (pclim) == 2
         clim(pclim);
+
+%         if( pclim(2) <=10)
+%             colorbar('eastoutside');
+%             c = colorbar;
+%             c.Label.String = "Grupos";
+%             c.Ticks = 1:pclim(2);
+%             c.TickLabels = 1:pclim(2);
+%             c.TickDirection = "out";
+%         end
+
     end 
     
-    c = colorbar;
-    c.Label.String = plabel;
 
     %dibuja el recuadro del mapa
     %m_grid('box','fancy','grid','none','fontsize',10);
-    m_grid('linewi',2,'tickdir','out');
+    m_grid('linestyle','none','box','fancy','tickdir','out');
+    %m_grid('linewi',2,'tickdir','out');
 
     % Regla de escala
     m_ruler([.05 .36],.1,3,'fontsize',7)
+    
     %m_northarrow(-101,23.5,.4,'type',2);
     % Estrella del norte 
     m_northarrow(-97.9,23.5,.4,'type',2);
 
-    %title(titulo);
-    
+    if(~isempty (titulo))
+        title(titulo);
+    end
 end
--- declaraciones.m
+++ declaraciones.m
@@ -6,8 +6,8 @@ disp(">>> Declarando variables...ok")
 dir_data = "D:\DATA\";
 
 %Rango de datos region a analizar de todo el municipio de tantoyuca
-lon_proyeccion = [-101.35 -97.70];
-lat_proyeccion = [ 19.05  23.96];
+%lon_proyeccion = [-101.35 -97.70];
+%lat_proyeccion = [ 19.05  23.96];
 
 %Ventana para el mapa region hidrografica rio panuco
 lon_mapa = [-101.6 -97.40];
@@ -31,3 +31,20 @@ ndvi_map_r = [ (33:80)  80*ones(1,79)  (80:-1:0)  zeros(1,48) ]' /80;  % red
 ndvi_map_g = flipud( ndvi_map_r );                                     % green
 ndvi_map_b = zeros( size( ndvi_map_r ) );                              % blue
 ndvi_colormap = [ ndvi_map_r  ndvi_map_g  ndvi_map_b ];
+
+% % colormap de elevacion 
+elevation_colormap = [
+    0.40 0.16 0.00
+    0.51 0.35 0.08
+    0.63 0.57 0.20
+    0.73 0.75 0.36
+    0.78 0.87 0.56
+    0.89 1.00 0.80
+    0.69 0.89 0.60
+    0.48 0.80 0.42
+    0.27 0.69 0.27
+    0.15 0.60 0.23
+    0.06 0.49 0.20
+    0.00 0.40 0.20];
+
+elevation_colormap = flipud(elevation_colormap);
